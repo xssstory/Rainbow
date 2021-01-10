0ffce6972716062590101e7c4354cf407b1d5b57
Yunfei Li <yunfeili.cloud@gmail.com>
2021-01-10 22:11:42 +0800

    Add scripts

---
+++ breakout_script/deploy_reset.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=$1 python main.py \
+	--id=breakout.deploy_whenreset_count_seed_$2 \
+	--env-type=atari \
+	--game=breakout \
+	--count-base-bonus=0.01 \
+        --seed=$2 \
+	--deploy-policy=reset \
+	--result-dir=/pvc/rainbow_lyf/results \
+	--checkpoint-interval=100000
---
+++ qbert_scripts/deploy_reset.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=$1 python main.py \
+	--id=qbert.deploy_whenreset_count_seed_$2 \
+	--env-type=atari \
+	--game=qbert \
+	--count-base-bonus=0.01 \
+        --seed=$2 \
+	--deploy-policy=reset \
+	--result-dir=/pvc/rainbow_lyf/results \
+	--checkpoint-interval=100000
