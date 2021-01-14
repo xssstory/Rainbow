a785cce4b6afd9d868700cc24bea6b232a6f1a26
Yunfei Li <yunfeili.cloud@gmail.com>
2021-01-14 08:12:52 +0000

    Add a bunch of scripts

---
+++ battle_zone_scripts/feature_pri_1e6.sh
@@ -0,0 +1,14 @@
+CUDA_VISIBLE_DEVICES=$1 python main.py \
+	--id=$0.$2 \
+	--env-type=atari \
+	--game=battle_zone \
+	--count-base-bonus=0.01 \
+        --seed=$2 \
+	--deploy-policy=dqn-feature \
+	--switch-memory-priority=1 \
+	--switch-sample-strategy=recent \
+	--switch-memory-capcacity=1000000 \
+	--switch-bsz=512 \
+	--feature-threshold=0.98 \
+        --result-dir=/pvc/rainbow_lyf/results \
+	--checkpoint-interval=100000
---
+++ road_scripts/reset+action_uniform_from_1e4.sh
@@ -0,0 +1,14 @@
+CUDA_VISIBLE_DEVICES=$1 python main.py \
+	--id=$0.$2 \
+	--env-type=atari \
+	--game=road_runner \
+	--count-base-bonus=0.01 \
+        --seed=$2 \
+	--deploy-policy=reset_policy \
+	--switch-memory-priority=0 \
+	--switch-sample-strategy=uniform \
+	--switch-memory-capcacity=10000 \
+	--switch-bsz=512 \
+	--policy-diff-threshold=0.5 \
+        --result-dir=/pvc/rainbow_lyf/results \
+	--checkpoint-interval=100000
