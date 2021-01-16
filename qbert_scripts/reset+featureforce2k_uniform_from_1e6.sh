2aa00401792fce67146e3e099a1bb6b629580cd3
Yunfei Li <yunfeili.cloud@gmail.com>
2021-01-16 15:50:42 +0000

    Add force 2k deploy scripts

---
+++ qbert_scripts/reset+featureforce2k_uniform_from_1e6.sh
@@ -0,0 +1,15 @@
+CUDA_VISIBLE_DEVICES=$1 python main.py \
+	--id=$0.$2 \
+	--env-type=atari \
+	--game=qbert \
+	--count-base-bonus=0.01 \
+        --seed=$2 \
+	--deploy-policy=reset_feature_force \
+        --force-interval=2000 \
+	--switch-memory-priority=0 \
+	--switch-sample-strategy=uniform \
+	--switch-memory-capcacity=1000000 \
+	--switch-bsz=512 \
+	--feature-threshold=0.98 \
+        --result-dir=/pvc/rainbow_lyf/results \
+	--checkpoint-interval=100000
