1147701366c9f54310c8a2fda2bc3bc441511ff6
Shusheng Xu <xssstory@gmail.com>
2021-01-19 10:20:21 +0800

    visited scripts

---
+++ battle_zone_scripts/visited_v2.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=$1 python main.py \
+	--id=$0.$2 \
+	--env-type=atari \
+	--game=battle_zone \
+	--count-base-bonus=0.01 \
+        --seed=$2 \
+	--deploy-policy=visited \
+        --result-dir=/pvc/rainbow_lyf/results \
+	--checkpoint-interval=100000
