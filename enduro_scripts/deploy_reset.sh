919a48f5fd67b6860906188b02c1b4dbe729033e
xssstory <33601810+xssstory@users.noreply.github.com>
2021-06-03 19:09:23 +0800

    Delete enduro_scripts directory

--- enduro_scripts/deploy_reset.sh
+++
@@ -1,9 +0,0 @@
-CUDA_VISIBLE_DEVICES=$1 python main.py \
-	--id=enduro.deploy_whenreset_count_seed_$2 \
-	--env-type=atari \
-	--game=enduro \
-	--count-base-bonus=0.01 \
-        --seed=$2 \
-	--deploy-policy=reset \
-	--result-dir=/pvc/rainbow_lyf/results \
-	--checkpoint-interval=100000
--- enduro_scripts/enduro.count.sh
+++
@@ -1,6 +0,0 @@
-python main.py \
-	--id=enduro_deploy_none_count \
-	--count-base-bonus=0.01 \
-	--env-type=atari \
-	--game=enduro \
-	--checkpoint-interval=100000 \
